c42eef4a14ae9412b31553f93c018d431bea60fc
Joakim Weise-Chiem <joakim.chiem@icloud.com>
2026-02-23 00:06:21 +0100

    feat(TransactionList): enable highlighting transaction list item

--- src/components/TransactionList/TransactionListItem.tsx
+++ src/components/TransactionList/TransactionListItem.tsx
@@ -2,6 +2,7 @@ import type { Transaction as TransactionInterface } from '../../ApiClient';
 
 interface TransactionListItemInterface extends TransactionInterface {
 	currency?: string;
+	highlight?: boolean;
 }
 
 export const TransactionListItem: React.FC<TransactionListItemInterface> = ({
@@ -9,9 +10,12 @@ export const TransactionListItem: React.FC<TransactionListItemInterface> = ({
 	amount,
 	// id,
 	currency = '€',
+	highlight,
 }) => {
 	return (
-		<li className="transaction-list-item">
+		<li
+			className={`transaction-list-item ${highlight ? 'transaction-list-item--highlighted' : ''}`}
+		>
 			<div className="transaction-list-item__content">
 				<h4 className="transaction-list-item__description">{description}</h4>
 				{/* <p className="transaction-list-item__id">{id}</p> */}
