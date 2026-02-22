945c7a2f1f7afc1ce36b9e5e6e63860f8fc7ad2e
Joakim Weise-Chiem <joakim.chiem@icloud.com>
2026-02-22 23:30:21 +0100

    fix(ApiClient): fix incorrect return type for getTransactionsById

--- src/ApiClient/index.ts
+++ src/ApiClient/index.ts
@@ -1,29 +1,37 @@
 // import { cards } from "./data/cards";
 // import { transactions } from "./data/transactions";
+
 export interface Card {
-  id: string;
-  description: string;
+	id: string;
+	description: string;
 }
 
 export interface Transaction {
-  id: string;
-  description: string;
-  amount: number;
+	id: string;
+	description: string;
+	amount: number;
+}
+
+export interface TransactionWithCardId extends Transaction {
+	cardId: string;
 }
 
 export async function getCards(): Promise<Card[]> {
-  const cards = await (await import("./data/cards.json")).default;
-  return cards;
+	const cards = (await import('./data/cards.json')).default;
+
+	return cards;
 }
 
-export async function getTransactions(cardId: string): Promise<Card[]> {
-  const transactions: Record<string, Transaction[]> = await (
-    await import("./data/transactions.json")
-  ).default;
+export async function getTransactionsByCardId(
+	cardId: string,
+): Promise<Transaction[]> {
+	const transactions: Record<string, Transaction[]> = (
+		await import('./data/transactions.json')
+	).default;
 
-  if (transactions[cardId]) {
-    return transactions[cardId];
-  }
+	if (transactions[cardId]) {
+		return transactions[cardId];
+	}
 
-  throw new Error("cardId not found");
+	throw new Error('cardId not found');
 }
