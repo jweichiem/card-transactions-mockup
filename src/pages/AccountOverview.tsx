52b1df2681bd259af4eff0e1e3f0a38fccd68ca2
Joakim Weise-Chiem <joakim.chiem@icloud.com>
2026-02-22 23:49:35 +0100

    feat(AccountOverview): add cards through useCards

--- src/pages/AccountOverview.tsx
+++ src/pages/AccountOverview.tsx
@@ -1,9 +1,18 @@
 import './AccountOverview.scss';
+import { Card } from '../components/Card';
+import { useCards } from '../hooks/useCards';
 
 const AccountOverview = () => {
+	const { data: cards } = useCards();
+
 	return (
 		<div className="main-layout">
 			<h1>Account Overview</h1>
+			<div className="row">
+				{cards.map((card) => (
+					<Card key={card.id} {...card} onClick={() => {}} />
+				))}
+			</div>
 		</div>
 	);
 };
